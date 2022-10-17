a11d1f3a385e213c8fe7b9f1f715ce3e08c27145
DhafinFawwaz <dhafin.fawwaz@gmail.com>
2022-10-17 19:26:55 +0700

    Added STATIC_URL

--- ProjectBook/ProjectBook/settings.py
+++ ProjectBook/ProjectBook/settings.py
@@ -44,6 +44,9 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+
+    'whitenoise.middleware.WhiteNoiseMiddleware',
+
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -119,6 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
+STATIC_ROOT = Path.join(BASE_DIR, 'staticfiles') # to deploy static files
 STATIC_URL = 'static/'
 
 # Default primary key field type
